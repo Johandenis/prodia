d3b3b936b8f7f6fb6d4437e211fe7c64baeef2c7
Johandenis <johandenisaguilarjdar2002@gmail.com>
2023-12-09 16:08:34 -0500

    denis
    ola

---
+++ config/APP.php
@@ -0,0 +1,8 @@
+<?php
+	const SERVERURL="http://localhost/prodia/";
+
+	const COMPANY="SISTEMAS PRODIA";
+
+	const MONEDA="$";
+
+	date_default_timezone_set('America/Lima');
---
+++ config/SERVER.php
@@ -0,0 +1,13 @@
+<?php
+
+	const SERVER="localhost";
+	const DB="prodia_db";
+	const USER="root";
+	const PASS="";
+
+	const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+
+	const METHOD="AES-256-CBC";
+	const SECRET_KEY='$prodia@2023';
+	const SECRET_IV='191170';
